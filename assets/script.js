55f1d9dbf21df0dd913de99443c2e3c2ffb8a917
Hedayat Abedijoo <hedayat.abedijoo@bookedby.com>
2022-04-20 11:15:43 +0200

    change question and more css

--- assets/script.js
+++ assets/script.js
@@ -5,28 +5,8 @@ const about = document.getElementById('about');
 let currentQuestionIndex = 0;
 var modal = document.getElementById("myModal");
 const googledriveurl = "https://drive.google.com/uc?export=view&id=";
-function getGoogleDriveImageUrl(key) {
-    switch (key) {
-        case "hedayat":
-            return "assets/images/hedayat.png";
-        case "buddy":
-            return "assets/images/buddy.png";
-        case "david":
-            return "assets/images/david.png";
-        case "tom":
-            return "assets/images/tom.png";
-        case "panda":
-            return "assets/images/panda.png";
-        case "quala":
-            return "assets/images/quala.png";
-        case "penguin":
-            return "assets/images/penguin.png";
-        default:
-            break;
-    }
-
-
-
+function getImage(key) {
+    return "assets/images/" + key + ".png";
 }
 btnplay.addEventListener('click', playGame);
 
@@ -41,7 +21,7 @@ function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.sort(() => Math.random() - .5).forEach(answer => {
         const img = document.createElement('img')
-        img.src = getGoogleDriveImageUrl(answer.text)
+        img.src = getImage(answer.text)
         img.alt = answer.text
         img.id = question.id
         img.classList.add('img-option')
@@ -96,10 +76,10 @@ const questions = [
         id: 1,
         question: 'Can you guess which one is Hedayat?',
         answers: [
-            { text: 'david', correct: true, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("hedayat")}'/> Wrong guess, This is Hedayat, you selected his good friend that will be introduced later.` },
-            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("hedayat")}'/> This one is Hedayat, actually you selected a good buddy! just wait! you will know him in a second.` },
-            { text: 'hedayat', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("hedayat")}'/> That is true, it is Hedayat.  did you know him or just guessed by face? ` },
-            { text: 'panda', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("hedayat")}'/> Hedayat loves panda, but unfortunately he is not one of them.  ` }
+            { text: 'david', correct: true, desc: `<img class='img-story' src='${getImage("hedayat")}'/> Wrong guess, This is Hedayat, you selected his good friend that will be introduced later.` },
+            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getImage("hedayat")}'/> This one is Hedayat, actually you selected a good buddy! just wait! you will know him in a second.` },
+            { text: 'hedayat', correct: false, desc: `<img class='img-story' src='${getImage("hedayat")}'/> That is true, it is Hedayat.  did you know him or just guessed by face? ` },
+            { text: 'panda', correct: false, desc: `<img class='img-story' src='${getImage("hedayat")}'/> Hedayat loves panda, but unfortunately he is not one of them.  ` }
         ],
         posttext: " ",
         button: "Go next!"
@@ -107,57 +87,57 @@ const questions = [
     {
         id: 2, question: 'Which one is David?',
         answers: [
-            { text: 'david', correct: true, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("david")}'/> Sooo smart, yeeh this is David with a big heart and a bigger head.` },
-            { text: 'tom', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("david")}'/> Really?!! Tom!!? This is David. Don't worry you were not so wrong, David has an internal Tom. If you don't believe it, just bite him. ` },
-            { text: 'david', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("david")}'/> Sooo smart, yeeh this is David with a big heart and a bigger head.` },
-            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("david")}'/> You selected a buddy who is a close friend of him. But this is David. He is always busy, even when he is not busy! ` }
+            { text: 'david', correct: true, desc: `<img class='img-story' src='${getImage("david")}'/> Sooo smart, yeeh this is David with a big heart and a bigger head.` },
+            { text: 'tom', correct: false, desc: `<img class='img-story' src='${getImage("david")}'/> Really?!! Tom!!? No No this one is David. Don't worry you were not so wrong, David has an internal Tom. If you don't believe it, just bite him. ` },
+            { text: 'david', correct: false, desc: `<img class='img-story' src='${getImage("david")}'/> Sooo smart, yeeh this is David with a big heart and a bigger head.` },
+            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getImage("david")}'/> You selected a buddy who is a close friend of him. But this one is David. He is always busy, even when he is not busy! ` }
         ],
         posttext: " ",
         button: "And then!"
     },
     {
-        id: 3, question: 'Please focous and find Buddy carefully. Otherwise!? something will explode.',
+        id: 3, question: 'Please focus and find Buddy carefully. Otherwise!? something will explode.',
         answers: [
-            { text: 'buddy', correct: true, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("buddy")}'/>  Yessss you did it. This is Buddy. With a lot of unpredictable abilities.` },
-            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("buddy")}'/> Yessss you did it. This is Buddy. With a lot of unpredictable abilities.` },
-            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("buddy")}'/> Yessss you did it. This is Buddy. With a lot of unpredictable abilities.` },
-            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("buddy")}'/> Yessss you did it. This is Buddy. With a lot of unpredictable abilities.` }
+            { text: 'buddy', correct: true, desc: `<img class='img-story' src='${getImage("buddy")}'/>  Yessss you did it. This is Buddy. With a lot of unpredictable abilities.` },
+            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getImage("buddy")}'/> Yessss you did it. This is Buddy. With a lot of unpredictable abilities.` },
+            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getImage("buddy")}'/> Yessss you did it. This is Buddy. With a lot of unpredictable abilities.` },
+            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getImage("buddy")}'/> Yessss you did it. This is Buddy. With a lot of unpredictable abilities.` }
         ],
         posttext: " ",
-        button: "Okay. More!"
+        button: "One more!"
     },
     {
         id: 4,
         question: 'Who deserve your F**K words if needed?',
         answers: [
-            { text: 'hedayat', correct: true, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("hedayat")}'/> Exactly, Hedayat takes responsibility for all the silly ideas in this container. Don't target others. ` },
-            { text: 'panda', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("hedayat")}'/> What? a lovely panda? No they didn't do anything here. Hedayat takes responsibility for all the silly ideas you see on this container. Don't target others. ` },
-            { text: 'tom', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("hedayat")}'/> Although Tom is the devil, but still Hedayat takes responsibility for all the silly ideas in this container. Don't target others.` },
-            { text: 'quala', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("hedayat")}'/>However hedayat's internal koala is big and sleepy all the time. But it is Hedayat that takes responsibility for all the silly ideas in this container. Don't target others.Please!  ` }
+            { text: 'hedayat', correct: true, desc: `<img class='img-story' src='${getImage("hedayat")}'/> Exactly, Hedayat takes responsibility for all the silly ideas in this container. Don't target others. ` },
+            { text: 'panda', correct: false, desc: `<img class='img-story' src='${getImage("hedayat")}'/> What? a lovely panda? No they didn't do anything here. Hedayat takes responsibility for all the silly ideas you see on this container. Don't target others. ` },
+            { text: 'tom', correct: false, desc: `<img class='img-story' src='${getImage("hedayat")}'/> Although Tom is the devil, but still Hedayat takes responsibility for all the silly ideas in this container. Don't target others.` },
+            { text: 'quala', correct: false, desc: `<img class='img-story' src='${getImage("hedayat")}'/>However hedayat's internal koala is big and sleepy all the time. But it is Hedayat that takes responsibility for all the silly ideas in this container. Don't target others.Please!  ` }
         ],
         posttext: " ",
         button: "Play again!"
     },
     {
         id: 5,
-        question: 'Commercial, Busy, Bridge, Money, Token, Challange, Connection. These tornifed words can be used to describe which one better?',
+        question: 'Commercial, Connection, Bridge, Business, Token, DAO, Blockchain, Holochain, Challenge, NFT. These tornifed words can be used to describe which one better?',
         answers: [
-            { text: 'david', correct: true, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("david")}'/> Absolutely correct, it is David. If you need any contact about commercial topics contact him.  ` },
-            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("david")}'/> however you tought about buddy, but the right & best person for of commercial topics is David. ` },
-            { text: 'hedayat', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("david")}'/> let's hedayat focus on the games and give the jobs of commercial topics to David.` },
-            { text: 'david', correct: true, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("david")}'/> Absolutely correct, it is David. So you got it right, if you need any contact about DAO, Token, Investment, Connection and etc whom to contact  ` },
+            { text: 'david', correct: true, desc: `<img class='img-story' src='${getImage("david")}'/> Absolutely correct, it is David who fits a lot with these topics: Commercial, Connection, Bridge, Business, Token, DAO, Blockchain, Holochain, Challenge, NFT and much more.  ` },
+            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getImage("david")}'/> Buddy can hold and support David to be fits for these topic: Commercial, Connection, Bridge, Business, Token, DAO, Blockchain, Holochain, Challenge, NFT and much more.  ` },
+            { text: 'hedayat', correct: false, desc: `<img class='img-story' src='${getImage("david")}'/> Hedayat would love to support David to be fits for these topic: Commercial, Connection, Bridge, Business, Token, DAO, Blockchain, Holochain, Challenge, NFT and much more.  .` },
+            { text: 'david', correct: true, desc: `<img class='img-story' src='${getImage("david")}'/> Absolutely correct, it is David who fits a lot with these topics: Commercial, Connection, Bridge, Business, Token, DAO, Blockchain, Holochain, Challenge, NFT and much more.  ` },
         ],
         posttext: " ",
-        button: "And then!"
+        button: "Next one!"
     },
     {
         id: 6,
-        question: 'Therapy, adventure, concious, holder, transformer, which one fits better to those words?',
+        question: 'Which one fits better to these words? Therapy, Adventure, Consciousness, Holder, Transformer',
         answers: [
-            { text: 'buddy', correct: true, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("buddy")}'/> It is buddy that support and hold others for transformation.` },
-            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("buddy")}'/> It is buddy that support and hold others for transformation.` },
-            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("buddy")}'/> It is buddy that support and hold others for transformation.` },
-            { text: 'buddy', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("buddy")}'/> It is buddy that support and hold others for transformation. ` },
+            { text: 'buddy', correct: true, desc: `<img class='img-story' src='${getImage("buddy")}'/> Amazingly, amazing part of Buddy is about consciousness & therapy with holding and supporting others in the transformation phase of their adventures. ` },
+            { text: 'buddy', correct: true, desc: `<img class='img-story' src='${getImage("buddy")}'/> Amazingly, amazing part of Buddy is about consciousness & therapy with holding and supporting others in the transformation phase of their adventures. ` },
+            { text: 'buddy', correct: true, desc: `<img class='img-story' src='${getImage("buddy")}'/> Amazingly, amazing part of Buddy is about consciousness & therapy with holding and supporting others in the transformation phase of their adventures. ` },
+            { text: 'buddy', correct: true, desc: `<img class='img-story' src='${getImage("buddy")}'/> Amazingly, amazing part of Buddy is about consciousness & therapy with holding and supporting others in the transformation phase of their adventures. ` },
         ],
         posttext: " ",
         button: "Finally!"
@@ -166,12 +146,12 @@ const questions = [
         id: 7,
         question: 'Who can be the next team member?',
         answers: [
-            { text: 'panda', correct: true, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("panda")}'/> If you would like to join us please bring your internal panda or any other animal.` },
-            { text: 'quala', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("quala")}'/> If you would like to join us please bring your internal quala or any other animal.` },
-            { text: 'tom', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("tom")}'/> If you would like to join us please bring your internal tom or any other animal.` },
-            { text: 'penguin', correct: false, desc: `<img class='img-story' src='${getGoogleDriveImageUrl("penguin")}'/> If you would like to join us please bring your internal penguin or any other animal.` },
+            { text: 'elephant', correct: true, desc: `<img class='img-story' src='${getImage("elephant")}'/> A strong elephant in the team would be great. Do you have it or know it?` },
+            { text: 'jerry', correct: false, desc: `<img class='img-story' src='${getImage("jerry")}'/> Playful Jerry is an exciting motivational option. Do you have it or know it?` },
+            { text: 'turtle', correct: false, desc: `<img class='img-story' src='${getImage("turtle")}'/>A slow turtle can be a good team member. Do you have it or know it?` },
+            { text: 'question', correct: false, desc: `<img class='img-story' src='${getImage("question")}'/> Hmm you also don't know who can be the next team member. Would you like to discover yourself more?.` },
         ],
-        posttext: "<br/> <br/> <h3>Thank you!</h3>",
+        posttext: "<br/> <br/> <h3>Thank you!</h3><h4><a href='http://www.zand.games/' target='blank'>ZAND.GAMES Teamtro</a><h4>",
         button: ""
     }
 ]
